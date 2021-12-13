c7fbb97a828c2427de9488a777983f60c7a6cff6
oezguensibm <Sinan.Oezguen@ibm.com>
2021-12-13 14:50:56 +0100

    Add multiple image support

--- src/auto_blur_image.py
+++ src/auto_blur_image.py
@@ -2,11 +2,14 @@
 
 import os
 import argparse
+from glob import glob
+
 import cv2
 from DetectorAPI import Detector
+import matplotlib.pyplot as plt
 
 
-def blurBoxes(image, boxes):
+def blurBoxes(image, boxes, kernel_size=5):
     """
     Argument:
     image -- the image that will be edited as a matrix
@@ -15,21 +18,21 @@ def blurBoxes(image, boxes):
     Returns:
     image -- the blurred image as a matrix
     """
-
+    
     for box in boxes:
         # unpack each box
         x1, y1 = box["x1"], box["y1"]
         x2, y2 = box["x2"], box["y2"]
-
+        
         # crop the image due to the current box
         sub = image[y1:y2, x1:x2]
-
+        
         # apply GaussianBlur on cropped area
-        blur = cv2.blur(sub, (25, 25))
-
+        blur = cv2.blur(sub, ksize=(kernel_size, kernel_size))
+        
         # paste blurred image on the original image
         image[y1:y2, x1:x2] = blur
-
+    
     return image
 
 
@@ -37,66 +40,75 @@ def main(args):
     # assign model path and threshold
     model_path = args.model_path
     threshold = args.threshold
-
+    
     # create detection object
     detector = Detector(model_path=model_path, name="detection")
-
+    
     # open image
-    image = cv2.imread(args.input_image)
-
-    # real face detection
-    faces = detector.detect_objects(image, threshold=threshold)
-
-    # apply blurring
-    image = blurBoxes(image, faces)
-
-    # show image
-    cv2.imshow('blurred', image)
-
-    # if image will be saved then save it
-    if args.output_image:
-        cv2.imwrite(args.output_image, image)
-        print('Image has been saved successfully at', args.output_image,
-              'path')
-    cv2.imshow('blurred', image)
-
-    # when any key has been pressed then close window and stop the program
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    for img_path in [i for s in [glob(os.path.join(args.images_dir, f'*.{img_type}')) for img_type in args.image_types] for i in s]:
+        image = cv2.imread(img_path)
+        
+        # real face detection
+        faces = detector.detect_objects(image, threshold=threshold)
+        
+        # apply blurring
+        image = blurBoxes(image, faces, kernel_size=args.kernel_size)
+        
+        # if image will be saved then save it
+        if args.output_dir is not None:
+            cv2.imwrite(os.path.join(args.output_dir, f'blurred_{os.path.basename(img_path)}'), image)
+            print('Image has been saved successfully at', args.output_dir, 'path')
+        else:
+            cv2.imshow('blurred', image)
+            # when any key has been pressed then close window and stop the program
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
     # creating argument parser
     parser = argparse.ArgumentParser(description='Image blurring parameters')
-
+    
     # adding arguments
     parser.add_argument('-i',
-                        '--input_image',
-                        help='Path to your image',
+                        '--images_dir',
+                        help='Path to your directory containing image',
                         type=str,
-                        required=True)
+                        default='../assets')
     parser.add_argument('-m',
                         '--model_path',
+                        default='../face_model/face.pb',
                         help='Path to .pb model',
-                        type=str,
-                        required=True)
+                        type=str)
     parser.add_argument('-o',
-                        '--output_image',
+                        '--output_dir',
                         help='Output file path',
+                        default='../outputs',
                         type=str)
     parser.add_argument('-t',
                         '--threshold',
                         help='Face detection confidence',
                         default=0.7,
                         type=float)
+    parser.add_argument('-k',
+                        '--kernel_size',
+                        help='Face detection confidence',
+                        default=30,
+                        type=int)
+    parser.add_argument('-s',
+                        '--image_types',
+                        nargs='+',
+                        default=['jpg', 'JPG', 'jpeg', 'JPEG', 'gif', 'png'],
+                        help='Image types to run through algorithm',
+                        type=str)
+    
     args = parser.parse_args()
     print(args)
     # if input image path is invalid then stop
-    assert os.path.isfile(args.input_image), 'Invalid input file'
-
+    assert os.path.isdir(args.images_dir), 'Invalid input file'
+    
     # if output directory is invalid then stop
-    if args.output_image:
-        assert os.path.isdir(os.path.dirname(
-            args.output_image)), 'No such directory'
-
+    if args.output_dir is not None and not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+    
     main(args)
